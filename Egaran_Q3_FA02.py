03c394b2b14b2a580a347ab3a5e9d0b1c6f5d583
AndrewLEgaran <andrewlester.egaran@smc.pshs.edu.ph>
2026-01-13 20:30:48 +0800

    Add files via upload
    My FA02 code and reflection.

---
+++ Egaran_Q3_FA02.py
@@ -0,0 +1,17 @@
+city1n2temp = [
+    [24, 26, 27, 25, 21, 29, 27,],
+    [32, 30, 29, 31, 33, 35, 34,]
+]
+
+#each row represents the temperatures
+#each column represents the city in order
+
+print(city1n2temp[0][4])
+city1n2temp[0][4] = 23
+print(city1n2temp[0][4])
+
+#Reflection
+"I chose the dataset of temperatures for two cities over a week because it is simple and easy to understand."
+"It allows me to demonstrate how to access and modify elements in a 2D list effectively."
+"It is also much more faster to do because I think it has less values to compute."
+"The 2D array helped me organize the data in a way that is easy to understand and access the value effectively and efficiently."
