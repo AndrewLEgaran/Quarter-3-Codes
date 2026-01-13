c3f46e3ac046e1a563cd7663c4629f113aa84f98
AndrewLEgaran <andrewlester.egaran@smc.pshs.edu.ph>
2026-01-13 20:44:40 +0800

    Add files via upload
    My FA03 code and short explanation

---
+++ Egaran_Q3_FA03.py
@@ -0,0 +1,24 @@
+city1n2temp = [
+    [24, 26, 27, 25, 21, 29, 27,],
+    [32, 30, 29, 31, 33, 35, 34,]
+]
+
+print(city1n2temp[0])
+print(city1n2temp[1])
+
+print(sum(city1n2temp[0]))
+print(sum(city1n2temp[1]))
+
+print(sum(city1n2temp[0]) / len(city1n2temp[0]))
+print(sum(city1n2temp[1]) / len(city1n2temp[1]))
+
+print(max(city1n2temp[0]))
+print(max(city1n2temp[1]))
+
+print(min(city1n2temp[0]))
+print(min(city1n2temp[1]))
+
+#Short Explanation
+"Using an array helped me organize and access the data more efficiently and effectively while having less lines."
+"It helped me be more organized with the data since it is less confusing to the eyes of the programmer."
+"There wasn't any particualr part of the data that was difficult to summarize since it was quite the easy data to understand."
