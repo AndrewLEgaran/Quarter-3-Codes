46fee8673045571db1c92af4bd156c6cfab9c50b
AndrewLEgaran <andrewlester.egaran@smc.pshs.edu.ph>
2026-01-25 21:49:59 +0800

    Enhance temperature data output with additional statistics
    Added print statements for total, average, maximum, and minimum temperatures.

--- Egaran_Q3_FA07.py
+++ Egaran_Q3_FA07.py
@@ -3,6 +3,7 @@ city1n2temp = [
     [32, 30, 29, 31, 33, 35, 34,]
 ]
 
-print("", sum(city1n2temp[0]), "\n", sum(city1n2temp[1]))
-print("", average_city1 := sum(city1n2temp[0]) / len(city1n2temp[0]), "\n", average_city2 := sum(city1n2temp[1]) / len(city1n2temp[1]))
-print("", max(city1n2temp[0]), "\n", max(city1n2temp[1]))
+print("Total for each row:", "\n", sum(city1n2temp[0]), "\n", sum(city1n2temp[1]))
+print("Average for each row:", "\n", average_city1 := sum(city1n2temp[0]) / len(city1n2temp[0]), "\n", average_city2 := sum(city1n2temp[1]) / len(city1n2temp[1]))
+print("Maximum of the entire data set:", "\n", max(max(city1n2temp[0]), max(city1n2temp[1])))
+print("Minimum of the entire data set:", "\n", min(min(city1n2temp[0]), min(city1n2temp[1])))
