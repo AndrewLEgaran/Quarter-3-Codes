a35a79ef5683cb25b4ccc41bd166a0644e37b2e7
AndrewLEgaran <andrewlester.egaran@smc.pshs.edu.ph>
2026-01-25 21:37:12 +0800

    FA07 - Total, Average, Maximum, and Minimum Values

---
+++ Egaran_Q3_FA07.py
@@ -0,0 +1,8 @@
+city1n2temp = [
+    [24, 26, 27, 25, 21, 29, 27,],
+    [32, 30, 29, 31, 33, 35, 34,]
+]
+
+print("", sum(city1n2temp[0]), "\n", sum(city1n2temp[1]))
+print("", average_city1 := sum(city1n2temp[0]) / len(city1n2temp[0]), "\n", average_city2 := sum(city1n2temp[1]) / len(city1n2temp[1]))
+print("", max(city1n2temp[0]), "\n", max(city1n2temp[1]))
